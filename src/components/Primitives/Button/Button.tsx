5a23b967f13339b2e9401df7b8e41663d4cb3870
Mikhail Ivanov <99988140+ivnvMkhl@users.noreply.github.com>
2023-01-11 00:49:36 +0300

    002_install_deps (#4)
    * 001 initial project (#1)
    
    * add react + typescript + vite
    
    * husky pre commit-hook
    
    * add .env support
    
    * add prettier
    
    * add prettier
    
    * add prettier
    
    Co-authored-by: mivanov <mivanov@ifotech-cloudsolutions.com>
    
    * 002_install_deps (#3)
    
    install eslnt, install react-query, install antd
    
    Co-authored-by: mivanov <mivanov@ifotech-cloudsolutions.com>
    
    Co-authored-by: mivanov <mivanov@ifotech-cloudsolutions.com>

---
+++ src/components/Primitives/Button/Button.tsx
@@ -0,0 +1,13 @@
+import { Button as AntdButton } from 'antd';
+import { ButtonProps as AntdButtonProps } from 'antd/lib/button/button';
+import React from 'react';
+
+type ButtonProps = AntdButtonProps;
+
+export const Button: React.FC<ButtonProps> = ({ children, className, ...restProps }) => {
+    return (
+        <AntdButton className={className} {...restProps}>
+            {children}
+        </AntdButton>
+    );
+};
