3c7c6dd117725cc905a428a69552e8cdefad597a
mivanov <mivanov@ifotech-cloudsolutions.com>
2023-01-16 00:06:12 +0300

    add antdForm proxy component, use Form in LoginPage and RegistrationPage

--- src/components/Complex/AuthorizationPage/components/RegistrationPage/RegistrationPage.tsx
+++ src/components/Complex/AuthorizationPage/components/RegistrationPage/RegistrationPage.tsx
@@ -1,37 +1,114 @@
 import React from 'react';
+import { AuthFormLabels } from '@components/Complex/AuthorizationPage/AuthorizationPage.constants';
 import { Button } from '@components/Primitives/Button';
 import { Divider } from '@components/Primitives/Divider';
 import { Input, Password } from '@components/Primitives/Input';
 import { Link, Title } from '@components/Primitives/Typography';
+import { Form, RuleRender } from '@src/components/Primitives/Form';
 import { useNavigate } from '@src/routing/Router';
 
 import styles from './RegistrationPage.module.css';
 
-type RegistrationPageProps = {};
+type RegistrationPageProps = {
+    onRegistrationSubmit?: (values: FormValues) => void;
+};
 
-const RegistrationPage: React.FC<RegistrationPageProps> = ({}) => {
+enum FormItem {
+    USERNAME = 'username',
+    EMAIL = 'email',
+    PASSWORD = 'password',
+    CONFIRM_PASSWORD = 'confirmPassword',
+}
+
+type FormValues = Record<FormItem, string>;
+
+const RegistrationPage: React.FC<RegistrationPageProps> = ({ onRegistrationSubmit }) => {
     const navigate = useNavigate();
     const handleLoginLink = () => navigate('/auth');
+    const validateConfirmPassword: RuleRender = ({ getFieldValue }) => ({
+        validator(_, value) {
+            if (!value || getFieldValue(FormItem.PASSWORD) === value) {
+                return Promise.resolve();
+            }
+            return Promise.reject(new Error('The two passwords that you entered do not match!'));
+        },
+    });
 
     return (
         <>
-            <div className={styles.authForm}>
+            <Form className={styles.authForm} layout="vertical" name="login" onFinish={onRegistrationSubmit}>
                 <Title level={2} type="secondary">
-                    Registration
+                    {AuthFormLabels.REGISTRATION}
                 </Title>
-                <Input placeholder="Email" />
-                <Input placeholder="Username" />
-                <Password placeholder="Password" />
-                <Password placeholder="Confirm password" />
-                <Button type="primary" block>
-                    Registration
-                </Button>
-            </div>
+                <Form.Item
+                    name={FormItem.EMAIL}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your E-mail',
+                        },
+                        {
+                            type: 'email',
+                            message: 'The input is not valid E-mail!',
+                        },
+                    ]}
+                >
+                    <Input placeholder={AuthFormLabels.EMAIL} />
+                </Form.Item>
+                <Form.Item
+                    name={FormItem.USERNAME}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your username',
+                        },
+                        {
+                            min: 4,
+                            message: 'Username min 4 symbols',
+                        },
+                    ]}
+                >
+                    <Input placeholder={AuthFormLabels.USERNAME} />
+                </Form.Item>
+                <Form.Item
+                    name={FormItem.PASSWORD}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please input your password',
+                        },
+                        {
+                            min: 8,
+                            message: 'Password min 8 symbols',
+                        },
+                    ]}
+                >
+                    <Password placeholder={AuthFormLabels.PASSWORD} />
+                </Form.Item>
+                <Form.Item
+                    name={FormItem.CONFIRM_PASSWORD}
+                    dependencies={[FormItem.PASSWORD]}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Confirm your password',
+                        },
+                        validateConfirmPassword,
+                    ]}
+                >
+                    <Password placeholder={AuthFormLabels.CONFIRM_PASSWORD} />
+                </Form.Item>
+                <Form.Item>
+                    <Button htmlType="submit" type="primary" block>
+                        {AuthFormLabels.REGISTRATION}
+                    </Button>
+                </Form.Item>
+            </Form>
             <div className={styles.orContainer}>
                 <Divider plain>or</Divider>
                 <div className={styles.actionsList}>
-                    <Link onClick={handleLoginLink}>Log in</Link>
-                    <Link disabled>Forgot Password?</Link>
+                    <Link onClick={handleLoginLink}>{AuthFormLabels.LOGIN}</Link>
+                    <Link disabled>{AuthFormLabels.FORGOT_PASSWORD}</Link>
                 </div>
             </div>
         </>
---
+++ src/components/Primitives/Form/Form.tsx
@@ -0,0 +1,7 @@
+import { Form as AntdForm } from 'antd';
+import { RuleObject, RuleRender } from 'antd/es/form';
+
+const Form = AntdForm;
+
+export { Form };
+export type { RuleObject, RuleRender };
